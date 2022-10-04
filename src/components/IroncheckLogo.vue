f1e5031e8e41dc77e66ba7f7fa36672eba3c31db
Nic <85948162+nicguzz@users.noreply.github.com>
2022-10-04 23:26:54 +0200

    Added Task Section in Home

--- src/components/IroncheckLogo.vue
+++ src/components/IroncheckLogo.vue
@@ -2,6 +2,7 @@
   <div class="image-container">
     <p class="todoapp-image"></p>
     <p class="text-5xl text-sky-500 font-bold">Iron cHeck</p>
+    <p class="atodoapp-desc">A To Do App</p>
   </div>
 </template>
 
