a6621678f631f63b1ea572ea7bcb6d1ad5f66903
Nic <85948162+nicguzz@users.noreply.github.com>
2022-10-04 12:54:37 +0200

    css applied to signin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "paper-color": "#d7c6bc",
+      },
+    },
   },
   plugins: [],
 };
