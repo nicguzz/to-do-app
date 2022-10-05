dded0c7b215d71bb8ae95174781a65de7a8afb19
Nic <85948162+nicguzz@users.noreply.github.com>
2022-10-05 17:06:11 +0200

    added notes

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -34,6 +34,7 @@ let editDescription = ref("");
 // FUNCTION THAT TOGGLES THE VISIBILITY OF THE INPUT
 
 function toggleEdit() {
+  // Function that will allow user to edit when editInput is changed to True(v-if="editInput")
   editInput.value = !editInput.value;
   editTitle.value = props.taskData.title;
   editDescription.value = props.taskData.description;
@@ -49,8 +50,10 @@ function errorhandling() {
 
 function edit() {
   if (editTitle.value === "") {
+    // If title is empty, error handling is started to inform user that string cannot be empty, activating the errorContainer to true and sending {{Error message}}
     errorhandling();
   } else {
+    // If title is not empty, if it has some value, then edit box is closed, error turns to false, a new object(oldToNew) is created and also an emit("editChild") to later be used in Home.vue (@editChild="editFather") with the function editFather set in Home.vue
     editInput.value = !editInput.value;
     errorContainer.value = !errorContainer.value;
     let oldToNew = {
@@ -58,19 +61,17 @@ function edit() {
       title: editTitle.value,
       description: editDescription.value,
     };
-    console.log(oldToNew);
     emit("editChild", oldToNew);
   }
 }
 
 function deleteTask() {
-  let deleteId = props.taskData.id;
-  emit("deleteChild", props.taskData);
-  console.log(props.taskData);
-  console.log(props.taskData.id);
+  emit("deleteChild", props.taskData); // function using an emit("deleteChild") to carry props.taskData to be used in Home.vue(@deleteChild="deleteId") and later use function deleteId to store to database
 }
 
 function completeItem() {
+  //This function issues an emit of the html query to be used in Home.vue with a new function changeToCompleted @emitItemComplete="changeToCompleted"
+
   emit("emitItemComplete", props.taskData);
 }
 
@@ -79,15 +80,3 @@ const props = defineProps(["taskData"]);
 </script>
 
 <style></style>
-
-<!-- 
-**Hints**
-1. ref() or reactive() can be used here to store the following, think if you want to store them either individually or like an object, up to you.
-2. Data properties need here are the following: a boolean to store a false**Important variable, a string to store an error, a string to store the value of the task that the user can edit, an initial false boolean to hide the inputFIeld used to edit the new task detail or details[this is in reference of the task title and the task description].
-3. Store the custom emit events that will be used to call the functions of the homeView for editing, deleting and toggling the status[completed, not complted] of the taskItem.
-4. Function to handle the error with the data properties used to control the error and the the boolean controlling the boolean empty variable.
-5. Function to handle the edit dialogue where the inputField is displayed and the string used to store the value of the inputField will be used here to save the value as a prop on this function.
-6. Function to emmit a custom event emit() that takes 2 parameters a name for the custom event and the value that will be send via the prop to the parent component. This function can control the toggle completion of the task on the homeview.
-7. Function to edit the task information that you decided that the user can edit. This function's body takes in a conditional that first checks if the value of the task [either title and description or just title] is empty which if true it runs the function used to handle the error on hint4. Else, the conditional sets the first mentioned boolean data property on hint2 back to its inital boolean value to hide the error message and repeat the same for the data property mentioned 4th on hint2; a constant that stores an object that holds the oldValue from the prop item and the value of the task coming from the data property mentioned 3rd on hint2; a custom event emit() that takes 2 parameters a name for the custom event and the value from the object used on this part of the conditional and lastly this part of the conditional sets the value of input field to an empty string to clear it from the ui. 
-8. Function to emmit a custom event emit() that takes 2 parameters a name for the custom event and the value that will be send via the prop to the parent component. This function can control the removal of  the task on the homeview.
--->
